dccc8f205ca1a2a876c13075c3ae702ebcb51ce0
JonathanTrenn <Jonathan_Trenn@interactivate-dc.com>
2026-08-13 15:21:09 -0400

    Add appId to sanity.cli.js to skip prompt on future deploys

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -6,6 +6,7 @@ export default defineCliConfig({
     dataset: 'production'
   },
   deployment: {
+    appId: 'ggbqt7vbc8l7epba8sctgbfm',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
